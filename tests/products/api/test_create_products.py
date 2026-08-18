1cdda9a4cb87731b0aa165f03404e3b038baaaad
Kwakic <61060709+Kwakic@users.noreply.github.com>
2026-08-18 18:47:33 +0200

    make test product fail (#28)

--- tests/products/api/test_create_products.py
+++ tests/products/api/test_create_products.py
@@ -54,7 +54,7 @@ def test_create_single_simple_product(
 
     product = create_valid_product()
 
-    product_id = product["kk"]
+    product_id = product["hhhhh"]
 
     # -------------------------------------------
     # Step 2 — Verify API response matches DB
